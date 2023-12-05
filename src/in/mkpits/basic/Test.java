7e0dcabf757d0043294e902e6e76a3528227935e
Shivani-Nagpure <shivaninagpure77@gmail.com>
2023-12-05 16:33:37 +0530

    Project Added

---
+++ src/in/mkpits/basic/Test.java
@@ -0,0 +1,14 @@
+package in.mkpits.basic;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Java");
+		System.out.println(4+8);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
